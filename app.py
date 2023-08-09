5ff6eefdb147059a97d517bf37f29a1a821b01e6
x2avi <106379965+x2avi@users.noreply.github.com>
2023-08-09 00:35:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
-import time
-
-import redis
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+@app.route('/pascal', methods=['POST'])
+def pascal():
+    n = int(request.args.get('n'))
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    return jsonify(triangle)
 
-@app.route('/')
-def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+if __name__ == '__main__':
+    app.run()
